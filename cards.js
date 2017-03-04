42b6c1a55f4e567a0af62ac40f92b6a12db890e4
nguyen,bao <bao.nguyen@ingramcontent.com>
2017-03-04 13:57:44 -0600

    done

--- cards.js
+++ cards.js
@@ -10,14 +10,22 @@ function add(){
 	var elemNo = ul.childElementCount;
 
 	var currList = ul.innerHTML;
-	var lineAdd = `<li><textarea style="width:50px;height:50px;border: 2px solid blue">${textInput}</textarea><button type="button" id="li${elemNo}">Delete</button></li>`;
-	// var lineAdd = `<li><textarea ${textInput}</textarea><button type="button" id="li${elemNo}">Delete</button></li>`;
+	
+	var lineAdd = `<li><textarea style="width:50px;height:50px;border: 2px solid blue">${textInput}</textarea><button type="button" class="delete">Delete</button></li>`;
+	
 	ul.innerHTML = currList + lineAdd;
-	var lii = `li${elemNo}`;
-	var a = document.getElementById(`li${elemNo}`);
-	a.addEventListener("click", function(e) {
-		this.parentNode.remove(e);})
 
 }
 
+
+function delCard(e) {
+	if (e.target.className == 'delete')
+	{
+		e.target.parentNode.remove();
+	}
+}
+
 document.getElementById("createBtn").addEventListener("click", add);
+document.body.addEventListener("click", delCard);
+
+
